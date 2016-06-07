29e8d7cc1eb2dabd702cc49ba49fe194608febe7
Peter B Smith <peterbutlersmith@gmail.com>
2016-06-07 17:39:41 -0400

    condensing pillarsItems reducers to a single file

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,33 @@
-import { combineReducers } from 'redux'
-import  pillarsItems from './pillarsItems'
+const pillarItem = (state, action) => {
+  switch(action.type) {
+    case 'ADD_PILLAR_ITEM':
+      return {
+        id: action.id,
+        pillar: action.pillar,
+        start: action.start,
+        duration: action.duration,
+        quality: action.quality,
+        notes: action.notes
+      };
+    default:
+      return state;
+  }
+}
 
-const pillarsApp = combineReducers({
-  pillarsItems
-})
+const pillarsItems = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_PILLAR_ITEM':
+      return [
+        ...state,
+        pillarItem(undefined, action)
+      ];
+    case 'REMOVE_PILLAR_ITEM':
+      return state.filter(todo =>
+        todo.id !== action.id
+      )
+    default:
+      return state;
+  }
+}
 
-export default pillarsApp
+export default pillarsItems
