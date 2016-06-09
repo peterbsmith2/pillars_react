c36427b5bf13d0cb2d77383593bd2ef0c2afb382
Peter B Smith <peterbutlersmith@gmail.com>
2016-06-09 15:43:10 -0400

    simplying root reducer to remove dynamic reducer creation

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,65 +1,23 @@
 import { combineReducers } from 'redux'
 import byId, * as fromById from './byId'
-import createList, * as fromList from './createList'
-import moment from 'moment'
+import listByDay, * as fromListByDay from './listByDay'
 
-const listOfDays =  {}
+const rootReducer = combineReducers({
+  byId,
+  listByDay
+})
 
-const createRootReducer = (day) => {
-  listOfDays[day] = createList(day)
+export default rootReducer
 
-  const listByDay = combineReducers({
-    ...listOfDays
-  })
-
-  return combineReducers({
-    byId,
-    listByDay
-  })
-}
-
-export default createRootReducer
 export const getDaysEntries = (state, day) => {
-  const ids = fromList.getIds(state.listByDay[day])
-  // TODO: the language below could be a little clearer
+  const ids = fromListByDay.getIds(state.listByDay, day)
   return ids.map(id => fromById.getEntries(state.byId, id))
 }
 
-export const getIsFetching = (state, day) =>
-  fromList.getIsFetching(state.listByDay[day])
+export const getIsFetching = (state, day) => {
+  return fromListByDay.getIsFetching(state.listByDay, day)
+}
 
-export const getErrorMessage = (state, day) =>
-  fromList.getErrorMessage(state.listByDay[day])
-
-// const pillarItem = (state, action) => {
-//   switch(action.type) {
-//     case 'ADD_PILLAR_ITEM':
-//       return {
-//         id: action.id,
-//         pillar: action.pillar,
-//         start: action.start,
-//         duration: action.duration,
-//         quality: action.quality,
-//         notes: action.notes
-//       };
-//     default:
-//       return state;
-//   }
-// }
-//
-// const pillarsItems = (state = [], action) => {
-//   switch(action.type) {
-//     case 'ADD_PILLAR_ITEM':
-//       return [
-//         ...state,
-//         pillarItem(undefined, action)
-//       ];
-//     case 'REMOVE_PILLAR_ITEM':
-//       return state.filter(todo =>
-//         todo.id !== action.id
-//       )
-//     default:
-//       return state;
-//   }
-// }
-
+export const getErrorMessage = (state, day) => {
+  return fromListByDay.getErrorMessage(state.listByDay, day)
+}
